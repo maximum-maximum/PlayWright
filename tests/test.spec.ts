4e1110c5fc8a9786ec48eca2cd8fbc9c92afe97e
maximum-maximum <mr_ver74@yahoo.co.jp>
2022-11-24 01:29:49 +0000

    update env's param name

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -6,12 +6,11 @@ dotenv.config();
 test("test", async ({ page }) => {
   await page.pause();
   await page.goto(process.env.END_POINT as string);
-  await page.locator("id=loginField").fill(process.env.USERNAME as string);
+  await page.locator("id=loginField").fill(process.env.NAME as string);
   await page.locator("id=passwordField").fill(process.env.PASSWORD as string);
   await page.locator("id=loginButton").click();
 
   await page.getByRole("link", { name: "Mobiusの使い方(デモ用)" }).click();
-
   await page.getByRole("link", { name: "Class User Manager" }).click();
   await page.getByRole("link", { name: "Import Users From Roster" }).click();
   await page.locator('input[name="file"]').click();
